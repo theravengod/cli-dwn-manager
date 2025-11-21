e2a2da0590262adde16349a0a937f46887d9f490
Adrian Popirlan <theravengod@gmail.com>
2025-11-21 19:15:57 +0200

    [WIP] [#7] HTTP Get call in a test (needed tokio)

--- dwn-manager-lib/src/lib.rs
+++ dwn-manager-lib/src/lib.rs
@@ -2,13 +2,29 @@ pub fn exec() {
     println!("Alive !")
 }
 
-/*#[cfg(test)]
+#[cfg(test)]
 mod tests {
-    use super::*;
-
+    // use cargo test --color=always --workspace -- --show-output
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn call_with_reqwest() {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+
+        rt.block_on(async {
+            let url = "https://mocki.io/v1/0e910579-8fd2-496c-8d1a-cf743041c020".to_string();
+            let client = reqwest::Client::new();
+
+            let body = client
+                .get(url)
+                .send()
+                .await
+                .expect("HTTP error")
+                .text()
+                .await
+                .unwrap();
+
+            println!("Got body: {:?}", body);
+
+            assert!(!body.is_empty());
+        });
     }
-}*/
+}
