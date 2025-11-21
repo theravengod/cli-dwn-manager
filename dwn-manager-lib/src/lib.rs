37304c7d0a784b77aa837030ab322f056ad1d5d4
Adrian Popirlan <theravengod@gmail.com>
2025-11-21 21:37:00 +0200

    [WIP] [#7] Format

--- dwn-manager-lib/src/lib.rs
+++ dwn-manager-lib/src/lib.rs
@@ -1,13 +1,17 @@
+use reqwest::blocking::Client;
 use std::fs::File;
 use std::io::copy;
-use reqwest::blocking::Client;
 
 pub fn exec() {
     println!("Alive !")
 }
 
 // Needs blocking client
-fn download_file(blocking_client: Client, url: &str, output_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn download_file(
+    blocking_client: Client,
+    url: &str,
+    output_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
     println!("Starting download from: {}", url);
 
     let mut response = blocking_client.get(url).send()?;
@@ -21,7 +25,7 @@ fn download_file(blocking_client: Client, url: &str, output_path: &str) -> Resul
 #[cfg(test)]
 mod tests {
     use super::*;
-    // run with cargo test --color=always --workspace -- --show-output
+    // run with: cargo test --color=always --workspace -- --show-output
     #[test]
     fn call_with_reqwest() {
         let url = "https://mocki.io/v1/0e910579-8fd2-496c-8d1a-cf743041c020";
