2be35f3774f5af3486569575151d1aeeb6992fde
Adrian Popirlan <theravengod@gmail.com>
2025-11-24 21:49:20 +0200

    [WIP] [#2] Not really handling errors

--- dwn-manager-lib/src/lib.rs
+++ dwn-manager-lib/src/lib.rs
@@ -1,9 +1,17 @@
+use colored::Colorize;
 use reqwest::blocking::Client;
 use std::fs::File;
 use std::io::copy;
 
 pub fn exec() {
-    println!("Alive !")
+    println!("Alive !");
+
+    let result = download_file(reqwest::blocking::Client::new(), "https://mocki.io/v1/0e910579-8fd2-496c-8d1a-cf743041c020", "./kpu.txt");
+    if result.is_ok() {
+        println!("{}", "Working !!!".bright_green());
+    } else {
+        println!("{}", format!("Failed {:?}", result.err().unwrap()).bright_red())
+    }
 }
 
 // Needs blocking client
@@ -12,12 +20,16 @@ fn download_file(
     url: &str,
     output_path: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    println!("Starting download from: {}", url);
-
-    let mut response = blocking_client.get(url).send()?;
+    let mut response = blocking_client.get(url).send();
     let mut output_file = File::create(output_path)?;
 
-    copy(&mut response, &mut output_file)?;
+    if let Err(e) = response {
+        let error_code = e.status().unwrap().as_u16();
+        eprintln!("{:?} Error downloading file", format!("[{}]", error_code).bright_red());
+        return Err(Box::new(e));
+    }
+
+    copy(response.as_mut().unwrap(), &mut output_file)?;
 
     Ok(())
 }
