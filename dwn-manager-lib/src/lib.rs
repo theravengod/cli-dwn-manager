63416879a430c8cd7a1db6095236833e1168fd1e
Adrian Popirlan <theravengod@gmail.com>
2025-11-25 22:40:53 +0200

    [WIP] [#2] Way better - now handles HTTP Error codes

--- dwn-manager-lib/src/lib.rs
+++ dwn-manager-lib/src/lib.rs
@@ -6,11 +6,14 @@ use std::io::copy;
 pub fn exec() {
     println!("Alive !");
 
-    let result = download_file(reqwest::blocking::Client::new(), "https://mocki.io/v1/0e910579-8fd2-496c-8d1a-cf743041c020", "./kpu.txt");
-    if result.is_ok() {
-        println!("{}", "Working !!!".bright_green());
-    } else {
-        println!("{}", format!("Failed {:?}", result.err().unwrap()).bright_red())
+    let result = download_file(reqwest::blocking::Client::new(), "https://mocki.io/v1/0e910579-8fd2-496c-8d1a-cf743041c0201", "./kpu.txt");
+    match result {
+        Ok(c_size) => {
+            println!("[✅ ] Downloaded {} bytes", c_size);
+        },
+        Err(e) => {
+            eprintln!("[❌ ] Something went wrong: {:?}", e);
+        }
     }
 }
 
@@ -19,11 +22,39 @@ fn download_file(
     blocking_client: Client,
     url: &str,
     output_path: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let mut response = blocking_client.get(url).send();
-    let mut output_file = File::create(output_path)?;
+) -> Result<(u64), Box<dyn std::error::Error>> {
+    let response = blocking_client.get(url).send();
 
-    if let Err(e) = response {
+    let (payload, err_payload) = match response {
+        Ok(val) => (Some(val), None),
+        Err(e) => (None, Some(e)),
+    };
+
+    if let Some(mut data) = payload {
+        println!("[✅ ] Got data !");
+        let status_code = data.status();
+        if status_code.is_success() {
+            println!("[✅ ] Got code: {}", status_code.as_u16());
+            let mut output_file = File::create(output_path)?;
+
+            let copy_result = copy(&mut data, &mut output_file);
+            if copy_result.is_ok() {
+                let copied_size = copy_result.unwrap();
+                println!("[✅ ] Copied successfully {} bytes", copied_size);
+                Ok(copied_size)
+            } else {
+                Err(Box::new(copy_result.err().unwrap()))
+            }
+        } else {
+            Err(format!("Received code: {}", status_code.as_u16()).into())
+        }
+    } else {
+        Err(Box::new(err_payload.unwrap()))
+    }
+
+
+
+    /*if let Err(e) = response {
         let error_code = e.status().unwrap().as_u16();
         eprintln!("{:?} Error downloading file", format!("[{}]", error_code).bright_red());
         return Err(Box::new(e));
@@ -31,7 +62,7 @@ fn download_file(
 
     copy(response.as_mut().unwrap(), &mut output_file)?;
 
-    Ok(())
+    Ok(())*/
 }
 
 #[cfg(test)]
