aeed1b3bcdc3824fc6bee2c2eadb3b388594df41
Adrian Popirlan <theravengod@gmail.com>
2025-12-01 22:16:47 +0200

    [WIP] [#2] Download with progress

--- dwn-manager-lib/src/lib.rs
+++ dwn-manager-lib/src/lib.rs
@@ -1,7 +1,8 @@
-use colored::Colorize;
-use reqwest::blocking::Client;
+use reqwest::blocking::{Client, Response};
+use reqwest::header::CONTENT_LENGTH;
+use std::error::Error;
 use std::fs::File;
-use std::io::copy;
+use std::io::{Read, Write};
 
 pub fn exec() {
     println!("Alive !");
@@ -9,8 +10,8 @@ pub fn exec() {
 
     let result = download_file(reqwest::blocking::Client::new(), good_url, "./kpu.txt");
     match result {
-        Ok(c_size) => {
-            println!("[✅ ] Downloaded {} bytes", c_size);
+        Ok(_) => {
+            println!("[✅ ] Successful");
         }
         Err(e) => {
             eprintln!("[❌ ] Something went wrong: {:?}", e);
@@ -23,7 +24,7 @@ fn download_file(
     blocking_client: Client,
     url: &str,
     output_path: &str,
-) -> Result<u64, Box<dyn std::error::Error>> {
+) -> Result<(), Option<Box<dyn std::error::Error>>> {
     let response = blocking_client.get(url).send();
 
     let (payload, err_payload) = match response {
@@ -32,6 +33,13 @@ fn download_file(
     };
 
     if let Some(mut data) = payload {
+        let total_size: u64 = data
+            .headers()
+            .get(CONTENT_LENGTH)
+            .and_then(|header| header.to_str().ok())
+            .and_then(|s| s.parse().ok())
+            .unwrap_or(0);
+
         println!("[✅ ] Got data !");
         let status_code = data.status();
         if status_code.is_success() {
@@ -40,20 +48,72 @@ fn download_file(
             let output_file = File::create(output_path);
             match output_file {
                 Ok(mut file) => {
-                    copy(&mut data, &mut file)
-                        .inspect(|size| println!("[✅ ] Copied successfully {} bytes", size))
-                        .map_err(Box::from)
+                    let progress_reporter = |downloaded: u64| {
+                        if total_size > 0 {
+                            let percentage = (downloaded as f64 / total_size as f64) * 100.0;
+                            println!("Downloaded {}% of file", percentage);
+                        } else {
+                            println!("Downloaded {} bytes", downloaded);
+                        }
+                    };
+                    copy_with_progress(&mut data, &mut file, progress_reporter)
                 }
                 Err(e) => {
-                    eprintln!("[❌ ] Could not create file at location {}: {}", output_path, e);
-                    Err(Box::from(e))
+                    eprintln!(
+                        "[❌ ] Could not create file at location {}: {}",
+                        output_path, e
+                    );
+                    Err(Some(Box::from(e)))
                 }
             }
         } else {
-            Err(format!("Received code: {}", status_code.as_u16()).into())
+            Err(Some(
+                format!("Received code: {}", status_code.as_u16()).into(),
+            ))
         }
     } else {
-        Err(Box::new(err_payload.unwrap()))
+        Err(Some(Box::new(err_payload.unwrap())))
+    }
+}
+
+fn copy_with_progress<F>(
+    input: &mut Response,
+    output: &mut File,
+    on_progress: F,
+) -> Result<(), Option<Box<dyn Error>>>
+where
+    F: Fn(u64) -> (),
+{
+    let mut buffer = [0; 8192];
+    let mut downloaded: u64 = 0;
+    let mut some_error: Option<Box<dyn Error>> = None;
+    loop {
+        let bytes_read = input
+            .read(&mut buffer)
+            .inspect(|b| println!("Transferred: {}", b))
+            .map_err(|e| eprintln!("Error occurred while copying: {}", e))
+            .unwrap();
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        let write_result = output.write_all(&buffer[..bytes_read]);
+
+        if write_result.is_err() {
+            some_error = Some(Box::from(write_result.err().unwrap()));
+            break;
+        } else {
+            downloaded += bytes_read as u64;
+            on_progress(downloaded);
+            std::io::stdout().flush().expect("Could not flush !"); // Might need to handle this error
+        }
+    }
+
+    if some_error.is_some() {
+        Err(some_error)
+    } else {
+        Ok(())
     }
 }
 
